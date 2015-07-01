0f72457fa774b8ef353dad170b7ac048a4d1dc77
cc <huanghaiyang@cc.local>
2015-07-01 11:43:12 +0800

    cc1

---
+++ ceshi/ceshi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ceshi
+//
+//  Created by cc on 15-7-1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"dajiba0");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
