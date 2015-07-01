e69d0f518a44c187e24faf66fdb4395a5a5d9680
cc <huanghaiyang@cc.local>
2015-07-01 11:45:23 +0800

    cc2

---
+++ ceshi/ceshi/ssss.h
@@ -0,0 +1,13 @@
+//
+//  ssss.h
+//  ceshi
+//
+//  Created by cc on 15-7-1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ssss : NSObject
+
+@end
---
+++ ceshi/ceshi/ssss.m
@@ -0,0 +1,13 @@
+//
+//  ssss.m
+//  ceshi
+//
+//  Created by cc on 15-7-1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ssss.h"
+
+@implementation ssss
+
+@end
